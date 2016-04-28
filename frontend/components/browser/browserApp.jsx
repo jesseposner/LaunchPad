032252f84ee83791a390b9dc21b0a52191503340
Jesse Posner <jesse.posner@gmail.com>
2016-04-28 10:46:58 -0700

    adds browserindex

--- frontend/components/browser/browserApp.jsx
+++ frontend/components/browser/browserApp.jsx
@@ -1,39 +1,15 @@
 var React = require('react'),
     CompanyStore = require('../../stores/companyStore'),
-    ClientActions = require('../../actions/clientActions');
+    ClientActions = require('../../actions/clientActions'),
+    BrowserIndex = require('./browserIndex');
 
 var BrowserApp = React.createClass({
-  getInitialState: function() {
-    return {
-      companies: []
-    };
-  },
-
-  componentDidMount: function() {
-    this.removeToken = CompanyStore.addListener(this.onChange);
-    ClientActions.fetchCompanies();
-  },
-
-  componentWillUnmount: function() {
-    this.removeToken.remove();
-  },
-
-  onChange: function () {
-    this.setState({
-      companies: CompanyStore.all()
-    });
-  },
 
   render: function() {
     return (
       <div>
         Browser
-        <p />
-        <ul>
-          {this.state.companies.map(function (company) {
-            return <li key={company.id}>{company.name}</li>;
-          })}
-        </ul>
+        <BrowserIndex />
       </div>
     );
   }
---
+++ frontend/components/browser/browserIndex.jsx
@@ -0,0 +1,41 @@
+var React = require('react'),
+    CompanyStore = require('../../stores/companyStore'),
+    ClientActions = require('../../actions/clientActions');
+
+var BrowserIndex = React.createClass({
+  getInitialState: function() {
+    return {
+      companies: []
+    };
+  },
+
+  componentDidMount: function() {
+    this.removeToken = CompanyStore.addListener(this.onChange);
+    ClientActions.fetchCompanies();
+  },
+
+  componentWillUnmount: function() {
+    this.removeToken.remove();
+  },
+
+  onChange: function () {
+    this.setState({
+      companies: CompanyStore.all()
+    });
+  },
+
+  render: function() {
+    return (
+      <div>
+        <ul>
+          {this.state.companies.map(function (company) {
+            return <li key={company.id}>{company.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+
+});
+
+module.exports = BrowserIndex;
